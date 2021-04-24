d3590a9d93aa258e3ff705d1c84717d17464dac3
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2021-04-24 10:40:20 +0530

    Wait for keypress before player spawn

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use bevy::prelude::*;
 use bevy::core::FixedTimestep;
 use bevy::sprite::collide_aabb::collide;
 
-use std::{thread, time};
 use rand::{thread_rng, Rng};
 
 const GRAVITY: f32 = 0.5;
@@ -11,18 +10,18 @@ const PLAYER_SIZE: (f32, f32) = (20., 20.);
 const BRICK_SIZE: (f32, f32) = (60., 20.);
 
 
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Default, Debug, PartialEq, Clone)]
 struct Materials {
     player_material: Handle<ColorMaterial>,
     brick_material: Handle<ColorMaterial>,
 }
 
-#[derive(Debug, PartialEq, Copy, Clone)]
+#[derive(Default, Debug, PartialEq, Copy, Clone)]
 struct Player {
     velocity: f32,
 }
 
-#[derive(Debug, PartialEq, Copy, Clone)]
+#[derive(Default, Debug, PartialEq, Copy, Clone)]
 struct Brick;
 
 fn setup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
@@ -33,11 +32,7 @@ fn setup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
     });
 }
 
-fn spawn_player(mut commands: Commands, materials: Res<Materials>) {
-    // FIXME: This is a hack allowing us to spawn the player after the bricks
-    //        start showing up. However, this causes the initial window to stay
-    //        stuck for 3s. Need to find a better way to achieve this.
-    thread::sleep(time::Duration::from_secs(3));
+fn spawn_player(commands: &mut Commands, materials: &Res<Materials>) {
     commands
         .spawn_bundle(SpriteBundle {
             material: materials.player_material.clone(),
@@ -51,7 +46,12 @@ fn spawn_player(mut commands: Commands, materials: Res<Materials>) {
         });
 }
 
-fn player_movement_input(keyboard_input: Res<Input<KeyCode>>, mut transformations: Query<&mut Transform, With<Player>>) {
+fn player_movement_input(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut transformations: Query<&mut Transform,
+    With<Player>>,
+    mut commands: Commands,
+    materials: Res<Materials>) {
     for key_pressed in keyboard_input.get_pressed() {
         // XXX: Not sure which one is the better choice here:
         // for mut transformation in animals.iter_mut() {
@@ -60,6 +60,10 @@ fn player_movement_input(keyboard_input: Res<Input<KeyCode>>, mut transformation
                 KeyCode::Left => transformation.translation.x -= 3.,
                 KeyCode::Right => transformation.translation.x += 3.,
                 _ => {},
+            }
+        } else {
+            match key_pressed {
+                _ => spawn_player(&mut commands, &materials),
             }
         }
     }
@@ -107,6 +111,7 @@ fn player_brick_collision(
     brick_transformations: Query<&Transform, With<Brick>>
 ) {
     if let Some(mut player) = player.iter_mut().next() {
+        // The collision should happen only when the player is coming down
         if player.velocity < 0. {
             if let Some(player_transformation) = player_transformation.iter().next() {
                 for brick_transformation in brick_transformations.iter() {
@@ -131,7 +136,6 @@ fn main() {
             ..Default::default()
         })
         .add_startup_system(setup.system())
-        .add_startup_stage("game_setup", SystemStage::single(spawn_player.system()))
         .add_system_set(
             SystemSet::new()
                 .with_run_criteria(FixedTimestep::step(1.5))
