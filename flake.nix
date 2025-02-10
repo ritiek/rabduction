2c81e101ecf293fb00a62b9dc2994766eab958c6
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2025-02-10 19:21:16 +0530

    Set $LD_LIBRARY_PATH when invoking makeRustPlatform

--- flake.nix
+++ flake.nix
@@ -42,6 +42,20 @@
             udev
             xorg.libxcb
           ];
+
+          postInstall = ''
+            wrapProgram $out/bin/${pname} \
+              --prefix LD_LIBRARY_PATH : "${pkgs.lib.makeLibraryPath (with pkgs; [
+                libGL
+                libxkbcommon
+                wayland
+                xorg.libX11
+                xorg.libXcursor
+                xorg.libXi
+                xorg.libXrandr
+                vulkan-loader
+              ])}"
+          '';
         };
 
         devShell = pkgs.mkShell {
